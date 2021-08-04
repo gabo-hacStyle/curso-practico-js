cce1d76add8dfac13cf494ed336870c5856ea8fc
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2021-08-04 17:02:49 -0500

    Terminando con las tres figuras

--- figuras-geometricas/figuras.js
+++ figuras-geometricas/figuras.js
@@ -9,14 +9,11 @@ const area = {
 };
     
 //Código del Cuadrado
-//Input donde el usuario digita el numero
-const input = document.getElementById('InputCuadrado');
-//Valor en entero
-const ladoCuadrado = parseInt(input.value);
+
 
 //Contenedores donde apareceran los valores
-const periContainer = document.getElementById('perime-container');
-const areaContainer = document.getElementById('area-container');
+//const periContainer = document.getElementById('perime-container');
+//const areaContainer = document.getElementById('area-container');
 
 //Funcion que permite hallar el perimetro
 const perimetroCuadrdo = (lado) => lado * 4;
@@ -27,43 +24,86 @@ const areaCuadrado = (lado) => lado * lado;
     container.innerHTML = `<span> ${termino} de: ${funcion(medidaCuadrado)}${medida}`;
 }*/
 const calcularPerimCuadrado = () => {
+    const input = document.getElementById('InputCuadrado');
+    //Valor en entero
+    const ladoCuadrado = parseInt(input.value);
+
     const perimetro = perimetroCuadrdo(ladoCuadrado);
     alert(`Tenemos que el perimetro es igual a ${perimetro}`);
 }
 const calcularAreaCuadrado = () => {
-    const area = areaCuadrdo(ladoCuadrado);
+    const input = document.getElementById('InputCuadrado');
+    //Valor en entero
+    const ladoCuadrado = parseInt(input.value);
+
+    const area = areaCuadrado(ladoCuadrado);
     alert(`Tenemos que el área es igual a ${area}`);
 }
 
 
 //Código del Triángulo
-/**
-console.group('Triangulos')
-const aTriangulo = 3;
-const bTriangulo = 4;
-const baseTriangulo = 9;
-const alturaTriangulo = 2.3;
 
-/**
-document.write(`Los lados miden: 
+
+/**document.write(`Los lados miden: 
 a: ${aTriangulo}cm, b: ${bTriangulo}cm, y la base: ${baseTriangulo}cm. `);
-console.log(`La altura es igual a ${alturaTriangulo}.`);
-
+console.log(`La altura es igual a ${alturaTriangulo}.`);*/
 const perimetroTriangulo = (ladoA, ladoB, ladoBase) => ladoA + ladoB + ladoBase;
 const areaTriangulo = (ladoBase, alturaTriangulo) => (ladoBase * alturaTriangulo)/2;
+/** document.write(` y el area es igual a ${areaTriangulo(baseTriangulo, alturaTriangulo)}`)
+document.write(`El triángulo tiene un perimetro de  ${perimetroTriangulo(aTriangulo, bTriangulo, baseTriangulo)}`);*/
 
-document.write(` y el area es igual a ${areaTriangulo(baseTriangulo, alturaTriangulo)}`)
-document.write(`El triángulo tiene un perimetro de  ${perimetroTriangulo(aTriangulo, bTriangulo, baseTriangulo)}`);
+const calcularPerimTriangulo = () =>{
+    //Lado a del triángulo
+    const inputA = document.getElementById('InputLadoA');
+    const ladoA = parseInt(inputA.value);
 
-console.groupEnd();
+    //Lado b del triángulo
+    const inputB = document.getElementById('InputLadoB');
+    const ladoB = parseInt(inputB.value); 
+    
+    // Base del triángulo 
+    const inputBase = document.getElementById('InputBaseTriangulo');
+    const base = parseInt(inputBase.value);
+
+    const perimetro = perimetroTriangulo(ladoA, ladoB, base);
+    alert(`Tenemos que el perimetro es igual a ${perimetro}`)
+}
+const calcularAreaTriangulo = () =>{
+    //Altura del triángulo
+    const inputAltura = document.getElementById('InputAlturaTriangulo');
+    const altura = parseInt(inputAltura.value)
+    
+    // Base del triángulo 
+    const inputBase = document.getElementById('InputBaseTriangulo');
+    const base = parseInt(inputBase.value);
+
+    const area = areaTriangulo(base, altura);
+    alert(`Tenemos que el área es igual a ${area}`)
+}
 
 //Código para el circulo
-console.group('Circulo');
-const radio = 12;
-const diametro = radio + radio;
+
 const pi = Math.PI;
 
-const perimetroCirculo = (diametro) => pi * diametro;
+const valorCircunferencia = (diametro) => pi * diametro;
 const areaCirculo = (radio) => (radio * radio) * pi;
 
-*/
+
+
+const calcularCircunferencia = () =>{
+    const input = document.getElementById('InputRadio');
+    const value = parseInt(input.value);
+    //Determinando el diametro
+    const diametro = value * 2;
+
+    console.log(diametro)
+    const circunferencia = valorCircunferencia(diametro);
+    alert(`Tenemos que el área es igual a ${circunferencia}`)
+}
+const calcularAreaCirculo = () =>{
+    const input = document.getElementById('InputRadio');
+    const value = parseInt(input.value);
+
+    const area = areaCirculo(value);
+    alert(`Tenemos que el área es igual a ${area}`)
+}
