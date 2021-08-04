55122796847caa4a436ed5f36a4da05fee0953ab
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2021-08-04 15:09:23 -0500

    Terminando cuadrado. No funciona en Firefox, provar en Chrome

--- figuras-geometricas/figuras.js
+++ figuras-geometricas/figuras.js
@@ -7,44 +7,37 @@ const area = {
     MEDIDA: 'cm^2',
     NOMBRE: 'Área'
 };
-
+    
 //Código del Cuadrado
 //Input donde el usuario digita el numero
-const input = document.getElementById('lado-cuadrado');
+const input = document.getElementById('InputCuadrado');
 //Valor en entero
-const medidaCuadrado = parseInt(input.value);
-//Botones 
-const btAreaCuadrado = document.getElementById('area-cuadrado-boton');
-const btPeriCuadrado = document.getElementById('perimetro-cuadrado-boton');
+const ladoCuadrado = parseInt(input.value);
+
 //Contenedores donde apareceran los valores
 const periContainer = document.getElementById('perime-container');
-const areaContainer = document.getElementById('area-container')
+const areaContainer = document.getElementById('area-container');
 
 //Funcion que permite hallar el perimetro
-const perimetroCuadrdo = (lado) =>{
-    const perimetro = lado * 4;
-    return perimetro;
-}
+const perimetroCuadrdo = (lado) => lado * 4;
 //Función que permite hallar el área
-const areaCuadrado = (lado) => {
-    const area = lado * lado;
-    return area;
-}
+const areaCuadrado = (lado) => lado * lado;
 //Función template* que muestra todo
-const muestra = (container, termino, funcion, medida) => {
+/**const muestra = (container, termino, funcion, medida) => {
     container.innerHTML = `<span> ${termino} de: ${funcion(medidaCuadrado)}${medida}`;
+}*/
+const calcularPerimCuadrado = () => {
+    const perimetro = perimetroCuadrdo(ladoCuadrado);
+    alert(`Tenemos que el perimetro es igual a ${perimetro}`);
 }
-//Muestra especificamente el permietro
-const muestraPerimetro = muestra(periContainer, perimetro.NOMBRE, perimetroCuadrdo, perimetro.MEDIDA);
-//Muestra especificamente el área
-const muestrArea = muestra(areaContainer, area.NOMBRE , areaCuadrado, area.MEDIDA);
-//ARREGLAR AQUI. LAS FUNCIONES NO TIENEN QUE VENIR CON () PORQUE O SINO SE EJECUTAN ANTES SIN EL addEventListener
-btAreaCuadrado.addEventListener('click', muestrArea);
-btPeriCuadrado.addEventListener('click', muestraPerimetro);
-
+const calcularAreaCuadrado = () => {
+    const area = areaCuadrdo(ladoCuadrado);
+    alert(`Tenemos que el área es igual a ${area}`);
+}
 
 
 //Código del Triángulo
+/**
 console.group('Triangulos')
 const aTriangulo = 3;
 const bTriangulo = 4;
@@ -56,14 +49,8 @@ document.write(`Los lados miden:
 a: ${aTriangulo}cm, b: ${bTriangulo}cm, y la base: ${baseTriangulo}cm. `);
 console.log(`La altura es igual a ${alturaTriangulo}.`);
 
-const perimetroTriangulo = (ladoA, ladoB, ladoBase) => {
-    const perimetro = ladoA + ladoB + ladoBase;
-    return perimetro;
-}
-const areaTriangulo = (ladoBase, alturaTriangulo) => {
-    const area = (ladoBase * alturaTriangulo)/2;
-    return area;
-}
+const perimetroTriangulo = (ladoA, ladoB, ladoBase) => ladoA + ladoB + ladoBase;
+const areaTriangulo = (ladoBase, alturaTriangulo) => (ladoBase * alturaTriangulo)/2;
 
 document.write(` y el area es igual a ${areaTriangulo(baseTriangulo, alturaTriangulo)}`)
 document.write(`El triángulo tiene un perimetro de  ${perimetroTriangulo(aTriangulo, bTriangulo, baseTriangulo)}`);
@@ -74,14 +61,9 @@ console.groupEnd();
 console.group('Circulo');
 const radio = 12;
 const diametro = radio + radio;
-const pi = 3.141579;
+const pi = Math.PI;
 
-const perimetroCirculo = (diametro) => {
-    const perimetro = pi * diametro;
-    return perimetro;
-}
-const areaCirculo = (radio) =>{
-    const area = (radio * radio) * pi;
-    return area;
-}
+const perimetroCirculo = (diametro) => pi * diametro;
+const areaCirculo = (radio) => (radio * radio) * pi;
+
 */
