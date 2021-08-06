fd81a3ac95b2e7e518132922090861c22055e9fc
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2021-08-06 13:00:48 -0500

    Finalizando el reto de los cupones

--- retos/cupones/cupones.js
+++ retos/cupones/cupones.js
@@ -1,31 +1,48 @@
+// Determinando el descuento de cada uno de los cupones 
 const cupones = {   
     cupon1: 10,
     cupon2: 15,
-    dads: 48
+    especial: 50
 }
+//Funcion de la formula matematica para calcular el precio con el descuento aplicado. Parametros el precio y el valor de descuento del cupon.
 const formula = (precio, cupon) => (precio * (100 - cupon)) / 100;
+//Funcion general encargrado de mostrar los resultados en el HTML. Parametros un contenedor y el resultado de la operacion matematica
+const mostrar =  (contenedor, resultado) => contenedor.innerText = `El resultado total con el descuento del cupon aplicado es de ${resultado} pesos`; 
+//Contenedor donde aparecerán los datos
+const contenedor = document.getElementById('ResultadoParrafo');
 
-const mostrar =  (resultado, contenedor) => contenedor.innerText = `El resultado total con el descuento del cupon aplicado es de ${resultado} pesos`; 
 
-const calcularDescuento = () => {
-    const contenedor = document.getElementById('ResultadoParrafo');
-    const precio = 30;
-    let dato = 'cupon2';
-    if(dato === cupones.cupon1 || dato === cupones.cupon2 || dato === cupones.dads){
-        if(dato === cupones.cupon1){
-            dato = cupones.cupon1;
-            const i = formula(precio, dato);
-            mostrar(i, contenedor);
-        } else if (dato === cupones.cupon2){
-            dato = cupones.cupon2;
-            const e = formula(precio, dato);
-            mostrar(e, contenedor);
-        } else if (dato === cupones.dads) {
-            dato = cupones.dads;
-            const f  = formula(precio, dato);
-            mostrar(f, contenedor);
-        }
-    } else {
-        contenedor.innerText = `Los datos colocados no son validos. Coloca otro cupón.`
-    }
+//Funcion especifica para el cupon1
+const descuentoCupon1 = () => {
+    //Valor introducido por el usuario y pasandolo a entero
+    const inputPrecio = document.getElementById('InputPrecio');
+    const precio = parseInt(inputPrecio.value);
+
+    //Calculando el resultado de acuerdo a la funcion formula()
+    const cupon1Resultado = formula(precio, cupones.cupon1);
+    //Para mostrar el resultado en pantalla
+    mostrar(contenedor, cupon1Resultado);
+}
+
+//Función especifica para el cupon 2
+const descuentoCupon2 = () => {
+
+    const inputPrecio = document.getElementById('InputPrecio');
+    const precio = parseInt(inputPrecio.value);
+
+    const cupon2Resultado = formula(precio, cupones.cupon2);
+    mostrar(contenedor, cupon2Resultado);
+
+}
+
+//Funcion especifica para el cupon especial
+const descuentoCuponEspecial = () => {
+
+    const inputPrecio = document.getElementById('InputPrecio');
+    const precio = parseInt(inputPrecio.value);
+
+    const cuponDasdResultado = formula(precio, cupones.especial);
+    
+    mostrar(contenedor, cuponDasdResultado);
+
 }
